e7c729c5c56c438f3ded67edcf2bf084bacc1fd4
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-28 17:31:15 -0300

    comentarios en TodoApp.tsx

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -8,7 +8,8 @@ export const TodoApp = () => {
   const handleAddTask = () => {
     if (nuevaTarea.trim() === "") return;
 
-    setListaTareas((tareasAnteriores) => [nuevaTarea, ...tareasAnteriores]);
+    // setListaTareas((tareasAnteriores) => [...tareasAnteriores, nuevaTarea]);
+    setListaTareas((tareasAnteriores) => [nuevaTarea, ...tareasAnteriores]);//nueva tarea siempre de primero en la lista
     setNuevaTarea("");
   };
 
@@ -32,44 +33,3 @@ export const TodoApp = () => {
     </div>
   );
 };
-
-//=========================================================================================
-//Codigo anterior original
-
-
-// import { useState } from 'react'
-// import { ListaTareas } from './ListaTareas'
-
-// export const TodoApp = () => {
-//   const [nuevaTarea, setNuevaTarea] = useState <string>('')
-//   const [listaTareas, setListaTareas] = useState <string[]>([])
-
-//   const handleAddTask = () => {
-//     if (nuevaTarea.trim() === '') return
-
-//     setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
-//     setNuevaTarea('')
-//   }
-
-//   const handleBorrarTarea = (index: number) => {
-//     setListaTareas(tareas => tareas.filter((_, i) => i !== index))
-//   }
-//   return (
-//     <div>
-//       <h1>Lista de tareas</h1>
-//       <div>
-//         <input
-//           type="text"
-//           value={ nuevaTarea }
-//           onChange={ (e)=>setNuevaTarea(e.target.value) }
-//           placeholder="Nueva Tarea"
-//         />
-//         <button
-//           onClick={handleAddTask}
-//         >Agregar Tarea</button>
-//       </div>
-//       <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}/>
-//     </div>
-//   )
-// }
-
