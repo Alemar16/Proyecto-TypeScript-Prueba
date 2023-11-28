cb500ca5baa407ac07a69e6687ff61df972eb0bf
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-28 13:03:04 -0300

    Se establece orden de la tarea

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,36 +1,75 @@
-import { useState } from 'react'
-import { ListaTareas } from './ListaTareas'
-
+import { useState } from "react";
+import { ListaTareas } from "./ListaTareas";
 
 export const TodoApp = () => {
-  const [nuevaTarea, setNuevaTarea] = useState <string>('')
-  const [listaTareas, setListaTareas] = useState <string[]>([])
+  const [nuevaTarea, setNuevaTarea] = useState<string>("");
+  const [listaTareas, setListaTareas] = useState<string[]>([]);
 
   const handleAddTask = () => {
-    if (nuevaTarea.trim() === '') return
-    
-    setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
-    setNuevaTarea('')
-  }
+    if (nuevaTarea.trim() === "") return;
+
+    setListaTareas((tareasAnteriores) => [nuevaTarea, ...tareasAnteriores]);
+    setNuevaTarea("");
+  };
 
   const handleBorrarTarea = (index: number) => {
-    setListaTareas(tareas => tareas.filter((_, i) => i !== index))
-  }
+    setListaTareas((tareas) => tareas.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <h1>Lista de tareas</h1>
       <div>
         <input
           type="text"
-          value={ nuevaTarea }
-          onChange={ (e)=>setNuevaTarea(e.target.value) }
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
           placeholder="Nueva Tarea"
         />
-        <button
-          onClick={handleAddTask} 
-        >Agregar Tarea</button>
+        <button onClick={handleAddTask}>Agregar Tarea</button>
       </div>
-      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}/> 
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} />
     </div>
-  )
-}
+  );
+};
+
+//=========================================================================================
+//Codigo anterior original
+
+
+// import { useState } from 'react'
+// import { ListaTareas } from './ListaTareas'
+
+// export const TodoApp = () => {
+//   const [nuevaTarea, setNuevaTarea] = useState <string>('')
+//   const [listaTareas, setListaTareas] = useState <string[]>([])
+
+//   const handleAddTask = () => {
+//     if (nuevaTarea.trim() === '') return
+
+//     setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
+//     setNuevaTarea('')
+//   }
+
+//   const handleBorrarTarea = (index: number) => {
+//     setListaTareas(tareas => tareas.filter((_, i) => i !== index))
+//   }
+//   return (
+//     <div>
+//       <h1>Lista de tareas</h1>
+//       <div>
+//         <input
+//           type="text"
+//           value={ nuevaTarea }
+//           onChange={ (e)=>setNuevaTarea(e.target.value) }
+//           placeholder="Nueva Tarea"
+//         />
+//         <button
+//           onClick={handleAddTask}
+//         >Agregar Tarea</button>
+//       </div>
+//       <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}/>
+//     </div>
+//   )
+// }
+
